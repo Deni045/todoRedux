068708ce4f874674442987cef14afe2ce79ce70e
Deni <06denidzh@gmail.com>
2022-12-20 17:06:46 +0300

    redux toto

--- src/index.js
+++ src/index.js
@@ -2,16 +2,61 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+ const initialState = [
+  {
+    text: 'JS',
+    done: true
+  },
+  {
+    text: 'React',
+    done: true
+  },
+  {
+    text: 'Html',
+    done: true
+  }
+ ]
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
+const reducer = (state = initialState, action) =>{
+  switch(action.type){
+   
+    case 'delete' :
+      return state.filter((item, index) =>{
+        if(action.payload === index){
+          return false
+        }else{
+          return true
+        }
+
+     
+
+      })
+
+
+
+
+    default:
+      return state
+  }
+}
+
+
+const store = createStore(reducer)
+
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<Provider store={store}>
+  <App />
+</Provider>
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
